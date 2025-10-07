9d677d92d9583ec13207879c42dc22213482a5ff
Keitiely Silva <keitielysilvaviana@gmail.com>
2025-10-07 12:06:25 -0300

    feat: Exercicio 2

---
+++ InstrumentosMusicais/src/main/java/com/mycompany/instrumentosmusicais/Bateria.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.instrumentosmusicais;
+
+/**
+ *
+ * @author keity
+ */
+public class Bateria extends InstrumentosMusicais{
+    private int qntdd;
+    
+    public Bateria(String nome, String material, int qntdd){
+        super(nome, material);
+        this.qntdd = qntdd;
+    }
+    
+    @Override
+    public void tocar(){
+        System.out.println("Tocando acordes do " + nome);
+    }
+    
+    @Override 
+    public void afinar(){
+        super.afinar();
+        System.out.println("Ajustando as cordas");
+    }
+    
+    @Override
+    public void mostrarInformações(){
+        super.mostrarInformações();
+        System.out.println("Quantidade de pecas: " + qntdd);
+    }
+    
+    //metodo para receber a qntdd de caixas
+    public int getQntdd(){
+        return qntdd;
+    }
+    
+}
---
+++ InstrumentosMusicais/src/main/java/com/mycompany/instrumentosmusicais/Piano.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.instrumentosmusicais;
+
+/**
+ *
+ * @author keity
+ */
+public class Piano extends InstrumentosMusicais{
+    private int teclas;
+    
+    public Piano(String nome, String material, int teclas){
+        super(nome, material);
+        this.teclas = teclas;
+    }
+    
+    @Override
+    public void tocar(){
+        System.out.println("Tocando acordes do " + nome);
+    }
+    
+    @Override 
+    public void afinar(){
+        super.afinar();
+        System.out.println("Ajustando as cordas");
+    }
+    
+    @Override
+    public void mostrarInformações(){
+        super.mostrarInformações();
+        System.out.println("Quantidade de teclas: " + teclas);
+    }
+   
+    public int getTeclas(){
+        return teclas;
+    }
+}
